a90b7bb7552da611575bf6a0763a4f0a9edd6aee
Kay Wu <quietelysium@gmail.com>
2017-05-26 22:13:03 -0400

    First release

---
+++ js/script.js
@@ -0,0 +1,34 @@
+$(window).scroll(function() {
+   if ($(this).scrollTop() > 280){  
+       $(".nav").addClass("sticky");
+   }
+   else{
+       $(".nav").removeClass("sticky");
+   }
+});
+
+
+jQuery(document).ready(function($) {
+    $(".scroll").click(function(event){     
+        event.preventDefault();
+        $('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
+    });
+});
+
+$(document).ready(function(){
+	//Check to see if the window is top if not then display button
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 100) {
+			$('#return-to-top').fadeIn();
+		} else {
+			$('#return-to-top').fadeOut();
+		}
+	});
+	
+	//Click event to scroll to top
+	$('#return-to-top').click(function(){
+		$('html, body').animate({scrollTop : 0},800);
+		return false;
+	});
+	
+});
